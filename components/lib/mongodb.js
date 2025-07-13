56c7ab795c314de069b1e6a8e9359656705b5125
Vihaan Thakur <chessblit@gmail.com>
2025-07-13 18:32:01 +0530

    manage mongodb connections efficiently

--- components/lib/mongodb.js
+++ components/lib/mongodb.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDB = async () => {
-    try {
-        const db = await mongoose.connect(process.env.MONGODB_URI, {
-            dbName: "ViTodo"
-        })
-        
-    } catch (error) {
-        throw new Error("MONGODB CONNECTION FAILED" + error)
-    }
+	try {
+		if (isConnected) return;
+
+		const db = await mongoose.connect(process.env.MONGODB_URI, {
+			dbName: "ViTodo",
+		});
+		isConnected = mongoose.connections[0].readyState === 1;
+	} catch (error) {
+		throw new Error("MONGODB CONNECTION FAILED" + error);
+	}
 };
 
 export default connectDB;
