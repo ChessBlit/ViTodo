a1b391e2e5545bf58f90fecf0b6fe3f78dd156f6
Vihaan Thakur <chessblit@gmail.com>
2025-07-13 18:39:14 +0530

    Make getAll route efficient

--- app/api/todos/getAll/route.js
+++ app/api/todos/getAll/route.js
@@ -6,40 +6,34 @@ import { cookies } from "next/headers";
 import { Todo } from "@/app/models/Todo.model";
 
 export async function POST() {
-    try {
-        await connectDB();
-        const cookieMonster = await cookies();
-        const refreshToken = cookieMonster.get("refreshToken")?.value;
-    
-        if (!refreshToken) throw new ApiError(401, "User is not logged in");
-    
-        const user = await User.findOne({
-            refreshToken,
-        });
-        
-    
-        const todoIds = await user.todos;
-    
-        const todos = await Promise.all(
-            todoIds.map(async (todoId) => {
-                return await Todo.findById(todoId);
-            })
-        );
-    
-    
-        return NextResponse.json({
-            status: 200,
-            message: "All todos fetched",
-            todos,
-        });
-    } catch (error) {
-        if (error instanceof ApiError){
-            return error.toNextResponse()
-        }
+	try {
+		await connectDB();
+		const cookieMonster = await cookies();
+		const refreshToken = cookieMonster.get("refreshToken")?.value;
 
-        return NextResponse.json({
-            status: 500,
-            message: "Internal server error while fetching todos"
-        })
-    }
+		if (!refreshToken) throw new ApiError(401, "User is not logged in");
+
+		const user = await User.findOne({
+			refreshToken,
+		}).lean();
+
+		const todoIds = await user.todos;
+
+		const todos = await Todo.find({ _id: { $in: todoIds } }).lean();
+
+		return NextResponse.json({
+			status: 200,
+			message: "All todos fetched",
+			todos,
+		});
+	} catch (error) {
+		if (error instanceof ApiError) {
+			return error.toNextResponse();
+		}
+
+		return NextResponse.json({
+			status: 500,
+			message: "Internal server error while fetching todos",
+		});
+	}
 }
